d653768655d949e68dc42774fb49369bddefb60c
himalAcharya <himalacharyayt@gmail.com>
2024-12-26 18:25:16 +0545

    initail commit

---
+++ passgen/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [length, setLength] = useState(8)
+  const [numallow,setnumallow]=useState(false)
+  const [charallow,setcharallow]=useState(false)
+  const [pass,setpass]=useState()
+  const passgen=()=>{
+
+  }
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default App
