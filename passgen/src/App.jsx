7fbea146790ae375c4a74fcc9345b8a8cdac8603
himalAcharya <himalacharyayt@gmail.com>
2024-12-27 21:37:53 +0545

    change

--- passgen/src/App.jsx
+++ passgen/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useCallback, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -8,12 +8,32 @@ function App() {
   const [numallow,setnumallow]=useState(false)
   const [charallow,setcharallow]=useState(false)
   const [pass,setpass]=useState()
-  const passgen=()=>{
+  const passgen=useCallback(()=>{
+    let pass=''
+    let str="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    if (numallow){
+      str+='0123456789'
+    }
+    if (charallow){
+      str+="!@#$%&*()"
+    }
+    for (let i = 0; i < length; i++) {
+      let char=Math.floor(Math.random()*str.length)
+      pass+=str[char]
+    }
+    setpass(pass)
 
-  }
+  },[length,numallow,charallow,setpass])
   return (
     <>
-    
+    <div className="body">
+    <div className="title">
+      <div className="box">
+      <p>Password Generator</p>
+        <input type="text" value={pass} />
+      </div>
+    </div>
+    </div>
     </>
   )
 }
